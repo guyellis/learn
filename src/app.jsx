4405a5350a7d80550324bdccd3c637bb1f4ab826
Guy Ellis <wildfiction@gmail.com>
2016-10-30 12:28:48 -0700

    Add options for + and -

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,52 @@
 require('bootstrap/dist/css/bootstrap.min.css');
 const React = require('react');
 const LearnMath = require('./learn-math');
+const LearnSetup = require('./setup');
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      lower: 0,
       setup: true,
+      sign: '+',
+      upper: 10,
     };
+    this.onChange = this.onChange.bind(this);
+    this.toggleSetup = this.toggleSetup.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  toggleSetup() {
+    this.setState({
+      setup: !this.state.setup,
+    });
   }
 
   render() {
+    const { setup } = this.state;
     return (
-      <LearnMath
-        lower={0}
-        sign={'+'}
-        upper={10}
-      />
+      <div>
+        {setup
+          ? <LearnSetup
+            lower={this.state.lower}
+            onChange={this.onChange}
+            sign={this.state.sign}
+            toggleSetup={this.toggleSetup}
+            upper={this.state.upper}
+          />
+          : <LearnMath
+            lower={this.state.lower}
+            sign={this.state.sign}
+            upper={this.state.upper}
+          />
+        }
+      </div>
     );
   }
 }
--- src/setup.jsx
+++ src/setup.jsx
@@ -1,18 +1,67 @@
-// const React = require('react');
+const React = require('react');
+// const TextField = require('material-ui/TextField').default;
+const DoneIcon = require('material-ui/svg-icons/action/done').default;
+const FloatingActionButton = require('material-ui/FloatingActionButton').default;
+const { RadioButton, RadioButtonGroup } = require('material-ui/RadioButton');
+const AddCircle = require('material-ui/svg-icons/content/add-circle').default;
+const AddCircleOutline = require('material-ui/svg-icons/content/add-circle-outline').default;
+const RemoveCircle = require('material-ui/svg-icons/content/remove-circle').default;
+const RemoveCircleOutline = require('material-ui/svg-icons/content/remove-circle-outline').default;
 
-// class LearnSetup extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
+// eslint-disable-next-line react/prefer-stateless-function
+class LearnSetup extends React.Component {
 
-//   render() {
-//     return (
-//       <div />
-//     );
-//   }
-// }
+  render() {
+    const {
+      onChange,
+      sign,
+      toggleSetup,
+    } = this.props;
+    const disabled = true;
+    return (
+      <div>
+        <h1>Settings</h1>
+        <RadioButtonGroup name="sign" defaultSelected={sign} onChange={onChange}>
+          <RadioButton
+            value="+"
+            checkedIcon={<AddCircle />}
+            uncheckedIcon={<AddCircleOutline />}
+          />
+          <RadioButton
+            value="-"
+            checkedIcon={<RemoveCircle />}
+            uncheckedIcon={<RemoveCircleOutline />}
+          />
+          <RadioButton
+            value="*"
+            checkedIcon={<AddCircle />}
+            uncheckedIcon={<AddCircleOutline />}
+            disabled={disabled}
+          />
+          <RadioButton
+            value="/"
+            checkedIcon={<AddCircle />}
+            uncheckedIcon={<AddCircleOutline />}
+            disabled={disabled}
+          />
+        </RadioButtonGroup>
+        <FloatingActionButton
+          onClick={toggleSetup}
+          title="Save and Go To Exercise"
+        >
+          <DoneIcon />
+        </FloatingActionButton>
+      </div>
+    );
+  }
+}
 
-// LearnSetup.propTypes = {
-// };
+LearnSetup.propTypes = {
+  // lower: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  sign: React.PropTypes.string.isRequired,
+  toggleSetup: React.PropTypes.func.isRequired,
+  // upper: React.PropTypes.number.isRequired,
+};
 
-// module.exports = LearnSetup;
+module.exports = LearnSetup;
