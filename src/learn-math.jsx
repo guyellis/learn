ccb2a6c592e36c14dbb67f7bf2b1473469f5dd21
Guy Ellis <wildfiction@gmail.com>
2016-10-31 20:58:35 -0700

    Add scores. Fixes #3

--- src/learn-math.jsx
+++ src/learn-math.jsx
@@ -3,12 +3,24 @@ const EditIcon = require('material-ui/svg-icons/editor/mode-edit').default;
 const FloatingActionButton = require('material-ui/FloatingActionButton').default;
 const React = require('react');
 const ResetIcon = require('material-ui/svg-icons/av/replay').default;
+const SaveIcon = require('material-ui/svg-icons/content/save').default;
 const TextField = require('material-ui/TextField').default;
+const { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } = require('material-ui/Table');
 
 const numberStyle = {
   fontSize: 'xx-large',
   margin: '10px',
 };
+
+function getScores() {
+  let scores;
+  try {
+    scores = JSON.parse(localStorage.getItem('scores') || '[]');
+  } catch (e) {
+    scores = [];
+  }
+  return scores;
+}
 
 class LearnMath extends React.Component {
   constructor(props) {
@@ -22,15 +34,17 @@ class LearnMath extends React.Component {
       startTime: Date.now(),
       totalCount: 0,
       upper: props.upper,
+      scores: getScores(),
     };
 
     this.checkAnswer = this.checkAnswer.bind(this);
+    this.focus = this.focus.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.onChange = this.onChange.bind(this);
     this.reset = this.reset.bind(this);
     this.runningTotal = this.runningTotal.bind(this);
+    this.save = this.save.bind(this);
     this.setNextTask = this.setNextTask.bind(this);
-    this.focus = this.focus.bind(this);
   }
 
   componentWillMount() {
@@ -106,6 +120,37 @@ class LearnMath extends React.Component {
     }
   }
 
+  save() {
+    const {
+      startTime,
+      correctCount,
+      totalCount,
+    } = this.state;
+    if (totalCount) {
+      const scores = getScores();
+      const {
+        lower,
+        sign,
+        upper,
+      } = this.props;
+      scores.unshift({
+        correctCount,
+        date: new Date().toISOString(),
+        lower,
+        sign,
+        time: Math.round((Date.now() - startTime) / 1000),
+        totalCount,
+        upper,
+      });
+      if (scores.length > 10) {
+        scores.pop();
+      }
+      localStorage.setItem('scores', JSON.stringify(scores));
+      this.setState({ scores });
+      this.reset();
+    }
+  }
+
   focus() {
     if (this.answerInput) {
       this.answerInput.focus();
@@ -159,7 +204,42 @@ class LearnMath extends React.Component {
     this.focus();
   }
 
+/*
+  <Table>
+    <TableBody>
+      <TableRow>
+        <TableRowColumn>1</TableRowColumn>
+        <TableRowColumn>John Smith</TableRowColumn>
+        <TableRowColumn>Employed</TableRowColumn>
+      </TableRow>
+*/
+
   render() {
+    const { scores = [] } = this.state;
+    const header = (
+      <TableHeader displaySelectAll={false}>
+        <TableRow>
+          <TableHeaderColumn>Date</TableHeaderColumn>
+          <TableHeaderColumn>Lower</TableHeaderColumn>
+          <TableHeaderColumn>Upper</TableHeaderColumn>
+          <TableHeaderColumn>Sign</TableHeaderColumn>
+          <TableHeaderColumn>Time</TableHeaderColumn>
+          <TableHeaderColumn>Correct</TableHeaderColumn>
+          <TableHeaderColumn>Total</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+    );
+    const rows = scores.map(score =>
+      (<TableRow key={score.date}>
+        <TableRowColumn>{score.date}</TableRowColumn>
+        <TableRowColumn>{score.lower}</TableRowColumn>
+        <TableRowColumn>{score.upper}</TableRowColumn>
+        <TableRowColumn>{score.sign}</TableRowColumn>
+        <TableRowColumn>{`${score.time}s`}</TableRowColumn>
+        <TableRowColumn>{score.correctCount}</TableRowColumn>
+        <TableRowColumn>{score.totalCount}</TableRowColumn>
+      </TableRow>)
+    );
     const checkStyle = {
       margin: '10px',
     };
@@ -218,20 +298,39 @@ class LearnMath extends React.Component {
         <div style={{ marginTop: '50px' }}>
           {'Created for Sonali and Kai.'}
         </div>
-        <FloatingActionButton
-          onClick={this.props.toggleSetup}
-          title="Edit Settings"
-          style={checkStyle}
-        >
-          <EditIcon />
-        </FloatingActionButton>
-        <FloatingActionButton
-          onClick={this.reset}
-          title="Edit Settings"
-          style={checkStyle}
-        >
-          <ResetIcon />
-        </FloatingActionButton>
+        <div>
+          <FloatingActionButton
+            onClick={this.props.toggleSetup}
+            title="Edit Settings"
+            style={checkStyle}
+          >
+            <EditIcon />
+          </FloatingActionButton>
+          <FloatingActionButton
+            onClick={this.reset}
+            title="Reset"
+            style={checkStyle}
+          >
+            <ResetIcon />
+          </FloatingActionButton>
+          <FloatingActionButton
+            onClick={this.save}
+            title="Save Results"
+            style={checkStyle}
+          >
+            <SaveIcon />
+          </FloatingActionButton>
+        </div>
+        <div>
+          {!!scores.length &&
+            <Table>
+              {header}
+              <TableBody displayRowCheckbox={false}>
+                {rows}
+              </TableBody>
+            </Table>
+          }
+        </div>
       </div>
     );
   }
