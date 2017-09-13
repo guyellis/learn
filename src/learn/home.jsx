8bffff601d6ffd6c2b66082dca8817047a6e74df
Guy Ellis <wildfiction@gmail.com>
2017-09-12 17:16:56 -0700

    Add GitHub logo and link to repo for Issues etc. Fixes #193

--- src/learn/home.jsx
+++ src/learn/home.jsx
@@ -1,22 +1,40 @@
-
-const React = require('react');
 const { Link } = require('react-router-dom');
 const RaisedButton = require('material-ui/RaisedButton').default;
+const React = require('react');
 
 const style = {
-  marginTop: '50px',
+  // marginTop: '50px',
+};
+
+const githubIconStyle = {
+  textAlign: 'right',
+  marginTop: '20px',
 };
 
 function home() {
   return (
-    <div style={style}>
-      <Link to="/math/drill">
-        <RaisedButton
-          label="Math Drill"
-          labelPosition="before"
-          primary
-        />
-      </Link>
+    <div>
+      <div style={githubIconStyle}>
+        <a
+          aria-label="Issue guyellis/learn on GitHub"
+          className="github-button"
+          data-size="large"
+          href="https://github.com/guyellis/learn/issues"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {'Issue'}
+        </a>
+      </div>
+      <div style={style}>
+        <Link to="/math/drill">
+          <RaisedButton
+            label="Math Drill"
+            labelPosition="before"
+            primary
+          />
+        </Link>
+      </div>
     </div>
   );
 }
