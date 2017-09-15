b0ede5442492bafa076ee772443175b9fd3512e9
Guy Ellis <wildfiction@gmail.com>
2017-09-15 16:54:22 -0700

    Add some DB methods for saving/getting scores. Issue #196

--- src/learn/db/index.js
+++ src/learn/db/index.js
@@ -1,13 +1,32 @@
 // const zango = require('zangodb');
 
+const MATH_DRILL_OPTIONS = 'mathDrillOptions';
+const MATH_DRILL_SCORES = 'mathDrillScores';
+
 class DB {
-  static saveOptions(options) {
-    localStorage.setItem('mathDrillOptions', JSON.stringify(options));
+  static setItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static getItem(key) {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : item;
+  }
+
+  static saveOptions(data) {
+    DB.setItem(MATH_DRILL_OPTIONS, data);
   }
 
   static getOptions() {
-    const opts = localStorage.getItem('mathDrillOptions');
-    return opts ? JSON.parse(opts) : opts;
+    return DB.getItem(MATH_DRILL_OPTIONS);
+  }
+
+  static saveScores(data) {
+    DB.setItem(MATH_DRILL_SCORES, data);
+  }
+
+  static getScores() {
+    return DB.getItem(MATH_DRILL_SCORES);
   }
 }
 
