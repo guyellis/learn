9f1a6105d3edf6ebd97afdbd3a1cd8e56058e90b
Guy Ellis <wildfiction@gmail.com>
2017-09-17 18:34:28 -0700

    Put dummy totals at top of scoreboard. Issue #209

--- src/learn/math/drill/scoreboard.jsx
+++ src/learn/math/drill/scoreboard.jsx
@@ -1,7 +1,7 @@
 // const db = require('../../db');
 // const Finished = require('./finished');
 const helper = require('./helper');
-// const moment = require('moment');
+const { List, ListItem } = require('material-ui/List');
 // const Options = require('./options');
 const React = require('react');
 const FloatingActionButton = require('material-ui/FloatingActionButton').default;
@@ -46,11 +46,35 @@ function badge(color, amount) {
     </FloatingActionButton>);
 }
 
+const badgeBoundaries = [
+  '2 seconds or less (per question)',
+  'between 2 and 3 seconds (per question)',
+  'between 3 and 4 seconds (per question)',
+  'more than 4 seconds (per question)',
+];
 
 function scoreboard() {
+  // TODO: Calculate total badges
+  const totalBadges = [15, 3, 7, 8];
+
   return (
     <div>
       <h3>{'Scoreboard'}</h3>
+      <List>
+        {
+          totalBadges.map((badgeTotal, index) => {
+            const primaryText =
+            `${badgeTotal} ${colorText[index]} Badge(s) - ${badgeBoundaries[index]}`;
+            return (
+              <ListItem
+                key={colorText[index]}
+                primaryText={primaryText}
+                leftIcon={badge(index, badgeTotal)}
+              />
+            );
+          })
+        }
+      </List>
       <Table>
         <TableHeader
           displaySelectAll={false}
