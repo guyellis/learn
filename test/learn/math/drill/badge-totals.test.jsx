faa34a0e0b98faae582433b70bf9d4d4cec2b8c2
Guy Ellis <wildfiction@gmail.com>
2017-09-25 16:03:55 -0700

    Add a test for BadgeTotals component with generated snapshot. Issue #223

---
+++ test/learn/math/drill/badge-totals.test.jsx
@@ -0,0 +1,19 @@
+
+const React = require('react');
+const BadgeTotals = require('../../../../src/learn/math/drill/badge-totals');
+const renderer = require('react-test-renderer');
+const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default;
+const getMuiTheme = require('material-ui/styles/getMuiTheme').default;
+const lightBaseTheme = require('material-ui/styles/baseThemes/lightBaseTheme').default;
+
+const muiTheme = getMuiTheme(lightBaseTheme);
+
+test('Badge totals should be rendered', () => {
+  const component = renderer.create(
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <BadgeTotals totals={[1, 2, 3]} />
+    </MuiThemeProvider>,
+  );
+  const badgeTotals = component.toJSON();
+  expect(badgeTotals).toMatchSnapshot();
+});
