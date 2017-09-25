7884bb2f426908efb019880c3788f95ab128de7b
Guy Ellis <wildfiction@gmail.com>
2017-09-24 20:47:01 -0700

    Add some more tests. Issue #223

--- test/learn/math/drill/test.helper.js
+++ test/learn/math/drill/test.helper.js
@@ -13,30 +13,113 @@ describe('Helper', () => {
     assert(pair[1] < 8);
   });
 
-  it.skip('should getScoreBarTimes()', () => {
-    // TODO: Need to mock the localStorage so that the title prop lines up
-    const actual = helper.getScoreBarTimes(0, [0]);
+
+  it('should getScoreBarTimes() for zero items', () => {
+    const scores = [];
+
+    const actual = helper.getScoreBarTimes(0, [0], scores);
+
+    const expected = [];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should getScoreBarTimes() for single item', () => {
+    const scores = [{
+      key: '00',
+      correctCount: 10,
+      date: 1,
+      timePerQuestion: 1.3,
+    }];
+
+    const actual = helper.getScoreBarTimes(0, [0], scores);
+
     const expected = [{
-      title: '9 hours ago',
+      date: 1,
       timePerQuestion: 1.3,
-    },
-    {
-      title: '4 hours ago',
-      timePerQuestion: 1.7,
-    },
-    {
-      title: '4 hours ago',
-      timePerQuestion: 1.3,
-    },
-    {
-      title: '3 hours ago',
-      timePerQuestion: 1.3,
-    },
-    {
-      title: '3 hours ago',
-      timePerQuestion: 1,
-    },
-    ];
+    }];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should sort getScoreBarTimes() for two items', () => {
+    const scores = [{
+      key: '00',
+      correctCount: 22,
+      date: 2,
+      timePerQuestion: 2.2,
+    }, {
+      key: '00',
+      correctCount: 11,
+      date: 1,
+      timePerQuestion: 1.1,
+    }];
+
+    const actual = helper.getScoreBarTimes(0, [0], scores);
+
+    const expected = [{
+      date: 1,
+      timePerQuestion: 1.1,
+    }, {
+      date: 2,
+      timePerQuestion: 2.2,
+    }];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should not return more than 5 items getScoreBarTimes() for six items', () => {
+    const scores = [{
+      key: '00',
+      correctCount: 66,
+      date: 6,
+      timePerQuestion: 6.6,
+    }, {
+      key: '00',
+      correctCount: 55,
+      date: 5,
+      timePerQuestion: 5.5,
+    }, {
+      key: '00',
+      correctCount: 44,
+      date: 4,
+      timePerQuestion: 4.4,
+    }, {
+      key: '00',
+      correctCount: 33,
+      date: 3,
+      timePerQuestion: 3.3,
+    }, {
+      key: '00',
+      correctCount: 22,
+      date: 2,
+      timePerQuestion: 2.2,
+    }, {
+      key: '00',
+      correctCount: 11,
+      date: 1,
+      timePerQuestion: 1.1,
+    }];
+
+    const actual = helper.getScoreBarTimes(0, [0], scores);
+
+    const expected = [{
+      date: 2,
+      timePerQuestion: 2.2,
+    }, {
+      date: 3,
+      timePerQuestion: 3.3,
+    }, {
+      date: 4,
+      timePerQuestion: 4.4,
+    }, {
+      date: 5,
+      timePerQuestion: 5.5,
+    }, {
+      date: 6,
+      timePerQuestion: 6.6,
+    }];
+
     assert.deepEqual(actual, expected);
   });
 });
