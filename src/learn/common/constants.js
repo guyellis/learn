84898bb52dc2f5f96849bef09308f85a0738f65d
Guy Ellis <wildfiction@gmail.com>
2017-09-26 16:15:01 -0700

    Add feeze to constants

--- src/learn/common/constants.js
+++ src/learn/common/constants.js
@@ -4,10 +4,10 @@ module.exports = {
   RECORD_EQUAL: 'RECORD_EQUAL',
   RECORD_MISS: 'RECORD_MISS',
   RECORD_NOT_EXIST: 'RECORD_NOT_EXIST',
-  COLOR_TEXT: ['Gold', 'Silver', 'Bronze', 'Blue'],
-  COLOR_HTML: ['#ffd700', '#c0c0c0', '#8C7853', 'lightblue'],
-  BADGE_BOUNDARIES: [0, 2, 3, 4], // Seconds per question boundaries for badges
-  ALPHABET: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-  OPERATIONS: ['+', '-', 'x', '\u00F7'],
-  OPERATION_NAMES: ['Addition', 'Subtraction', 'Multiplication', 'Division'],
+  COLOR_TEXT: Object.freeze(['Gold', 'Silver', 'Bronze', 'Blue']),
+  COLOR_HTML: Object.freeze(['#ffd700', '#c0c0c0', '#8C7853', 'lightblue']),
+  BADGE_BOUNDARIES: Object.freeze([0, 2, 3, 4]), // Seconds per question boundaries for badges
+  ALPHABET: Object.freeze('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')),
+  OPERATIONS: Object.freeze(['+', '-', 'x', '\u00F7']),
+  OPERATION_NAMES: Object.freeze(['Addition', 'Subtraction', 'Multiplication', 'Division']),
 };
