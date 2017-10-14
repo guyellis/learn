c51b34ec9275fa101abb4b27e64524e617391cc5
Guy Ellis <wildfiction@gmail.com>
2017-10-14 16:56:26 -0700

    Add test for Help page. Issue #223

---
+++ test/learn/help/index.test.jsx
@@ -0,0 +1,25 @@
+const React = require('react');
+const Help = require('../../../src/learn/help');
+const renderer = require('react-test-renderer');
+const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default;
+const getMuiTheme = require('material-ui/styles/getMuiTheme').default;
+const lightBaseTheme = require('material-ui/styles/baseThemes/lightBaseTheme').default;
+const {
+  MemoryRouter,
+} = require('react-router-dom');
+
+const muiTheme = getMuiTheme(lightBaseTheme);
+
+describe('Help', () => {
+  test('should render the Help component', () => {
+    const component = renderer.create(
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <MemoryRouter>
+          <Help />
+        </MemoryRouter>
+      </MuiThemeProvider>);
+
+    const app = component.toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
