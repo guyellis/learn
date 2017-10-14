de9c15258dafa0544a773173559c8c4ee8581863
Guy Ellis <wildfiction@gmail.com>
2017-10-14 08:28:29 -0700

    Add more coverage to getScoreBarTimes(). Issue #223

--- test/learn/math/drill/helper.test.js
+++ test/learn/math/drill/helper.test.js
@@ -33,6 +33,16 @@ describe('Helper', () => {
     expect(pair[1]).toBeLessThan(8);
   });
 
+  test('should default to [] when scores undefined in getScoreBarTimes()', () => {
+    db.getScores.mockReturnValueOnce(undefined);
+
+    const actual = helper.getScoreBarTimes(0, [0]);
+
+    const expected = [];
+
+    expect(actual).toEqual(expected);
+  });
+
   test('should getScoreBarTimes() for zero items', () => {
     const scores = [];
 
