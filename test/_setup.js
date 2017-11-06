97a380851839699feb9e0983c91d7d789c33cb04
Guy Ellis <wildfiction@gmail.com>
2017-11-05 18:33:40 -0700

    Prevent tests from passing if propTypes fail. Issue #223

--- test/_setup.js
+++ test/_setup.js
@@ -11,3 +11,14 @@ global.localStorage = {
 global.requestAnimationFrame = (done) => {
   setTimeout(done, 0);
 };
+
+// Prevent tests from passing if a prop-type is not correct by
+// throwing an error instead of doing a console.error()
+const { error } = console;
+// eslint-disable-next-line no-console
+console.error = (warning, ...args) => {
+  if (/(Invalid prop|Failed prop type)/gi.test(warning)) {
+    throw new Error(warning);
+  }
+  error.apply(console, [warning, ...args]);
+};
